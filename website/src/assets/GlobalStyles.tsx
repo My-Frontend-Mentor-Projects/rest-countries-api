dbfc77354b2ffbd6c7eeeb9eb16b7fa63dad3db9
pandaman404 <valenzuela.ahumada4@gmail.com>
2023-09-08 17:34:46 -0300

    config pre-deploy

--- website/src/assets/GlobalStyles.tsx
+++ website/src/assets/GlobalStyles.tsx
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 import { device } from './breakpoints';
 
-export const GlobalStyles = createGlobalStyle`
+export const GlobalStyles: any = createGlobalStyle`
 
 #root {
   max-width: 1920px;
