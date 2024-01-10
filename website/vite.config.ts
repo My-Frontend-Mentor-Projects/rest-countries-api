3460c9fdb85cf6ea9a561a752648207382e1fbd3
pandaman404 <valenzuela.ahumada4@gmail.com>
2024-01-10 20:29:09 -0300

    jio

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/pandaman404/rest-countries-spa',
+  base: '/rest-countries-spa/',
   plugins: [react()],
 });
